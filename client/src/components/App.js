ac88a9df3e658ffdf8faceee1bff6e1e54e66c5e
Praveen Kumar <praveenscience@gmail.com>
2021-05-09 18:18:07 +0100

    Converted App into Stateful Class Component.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,23 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 import Navbar from "./Bootstrap/Navbar";
 import List from "./DateList/List";
 import { WeatherAppContents } from "../constants/Dates";
 
-const App = () => {
-  return (
-    <div className="App">
-      <Navbar className="Header justify-content-center" dark={true}>
-        Weather App
-      </Navbar>
-      <div className="container mt-4">
-        <div className="row">
-          <div className="col-4">
-            <List WeatherAppContents={WeatherAppContents} />
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar className="Header justify-content-center" dark={true}>
+          Weather App
+        </Navbar>
+        <div className="container mt-4">
+          <div className="row">
+            <div className="col-4">
+              <List WeatherAppContents={WeatherAppContents} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
