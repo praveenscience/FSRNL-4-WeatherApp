a2564e14a9399b498ceea85e4dc12ddfb8d23e1a
Praveen Kumar <praveenscience@gmail.com>
2021-05-09 18:08:47 +0100

    Updated App with contents.
    * Added NavBar.
    * Added Days list.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import Navbar from "./Bootstrap/Navbar";
+import List from "./DateList/List";
+import { WeatherAppContents } from "../constants/Dates";
 
 const App = () => {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <Navbar className="Header justify-content-center" dark={true}>
+        Weather App
+      </Navbar>
+      <div className="container mt-4">
+        <div className="row">
+          <div className="col-4">
+            <List WeatherAppContents={WeatherAppContents} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
