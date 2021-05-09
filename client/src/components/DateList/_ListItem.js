9026064e91c69e587ec8654a3d845410838dfe63
Praveen Kumar <praveenscience@gmail.com>
2021-05-09 18:17:13 +0100

    Replaced active item with success.
    * Changed active to success list item.
    * Added the Weekday using a simple math.

--- client/src/components/DateList/_ListItem.js
+++ client/src/components/DateList/_ListItem.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { DaysAgo } from "../../constants/Dates";
+import { DaysAgo, WeekDays } from "../../constants/Dates";
 
 const ListItem = ({ CurDate }) => {
   const today = new Date().getDate();
@@ -8,14 +8,21 @@ const ListItem = ({ CurDate }) => {
       href="#"
       className={
         "list-group-item list-group-item-action" +
-        (CurDate === today ? " active" : "")
+        (CurDate === today ? " list-group-item-success" : "")
       }
     >
       <div className="d-flex w-100 justify-content-between">
         <h5 className="mb-1">May {CurDate}</h5>
         <small>{DaysAgo(CurDate - today)}</small>
       </div>
-      <p className="mb-1">Hi</p>
+      <p className="mb-1">
+        {
+          WeekDays[
+            (WeekDays.length * 5 + (new Date().getDay() + CurDate - today)) %
+              WeekDays.length
+          ]
+        }
+      </p>
       <small>And some small print.</small>
     </a>
   );
