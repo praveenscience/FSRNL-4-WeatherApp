62434dcff29d9094ddf2b45b94f87f61409a4d1c
Praveen Kumar <praveenscience@gmail.com>
2021-05-15 18:45:19 +0100

    Encapsulated App inside BrowserRouter.

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
@@ -7,9 +8,11 @@ import reportWebVitals from "./reportWebVitals";
 import "./styles/index.scss";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>,
   document.getElementById("root")
 );
 
