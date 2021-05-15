6346957d81f76d393ca8d5b14b14ae15829260b5
Praveen Kumar <praveenscience@gmail.com>
2021-05-15 18:51:09 +0100

    Converted App into Functional Component.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,48 +1,39 @@
-import React, { Component } from "react";
 import Navbar from "./Bootstrap/Navbar";
 import List from "./DateList/List";
 import { WeatherAppContents } from "../constants/Dates";
 import Form from "./MainView/Form/Form";
 
-class App extends Component {
-  state = {
-    SelectedDay: -1
+const App = () => {
+  const SelectedDay = -1;
+  const handleSelectedDayChange = e => handleSelectedDayClick(e.target.value);
+  const handleSelectedDayClick = SelectedDay => {
+    // Do something with Selected Day.
   };
-  handleSelectedDayChange = e => {
-    this.handleSelectedDayClick(e.target.value);
-  };
-  handleSelectedDayClick = SelectedDay => {
-    this.setState({
-      SelectedDay
-    });
-  };
-  render() {
-    return (
-      <div className="App">
-        <Navbar className="Header justify-content-center" dark={true}>
-          Weather App
-        </Navbar>
-        <div className="container mt-4">
-          <div className="row">
-            <div className="col-4">
-              <List
-                WeatherAppContents={WeatherAppContents}
-                SelectedDay={this.state.SelectedDay}
-                handleSelectedDayClick={this.handleSelectedDayClick}
-              />
-            </div>
-            <div className="col-8">
-              <Form
-                SelectedDay={this.state.SelectedDay}
-                handleSelectedDayChange={this.handleSelectedDayChange}
-                WeatherAppContents={WeatherAppContents}
-              />
-            </div>
+  return (
+    <div className="App">
+      <Navbar className="Header justify-content-center" dark={true}>
+        Weather App
+      </Navbar>
+      <div className="container mt-4">
+        <div className="row">
+          <div className="col-4">
+            <List
+              WeatherAppContents={WeatherAppContents}
+              SelectedDay={SelectedDay}
+              handleSelectedDayClick={handleSelectedDayClick}
+            />
+          </div>
+          <div className="col-8">
+            <Form
+              SelectedDay={SelectedDay}
+              handleSelectedDayChange={handleSelectedDayChange}
+              WeatherAppContents={WeatherAppContents}
+            />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
